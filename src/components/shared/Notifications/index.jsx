2a04ef9a24f9207396ea5887003d94b5ca3336c0
Sergio Mercado <mercadosergio754@gmail.com>
2025-08-19 17:52:34 -0500

    class change

--- src/components/shared/Notifications/index.jsx
+++ src/components/shared/Notifications/index.jsx
@@ -33,13 +33,6 @@ export function Notifications() {
       <DropdownMenuTrigger asChild>
         <Button variant='ghost' size='sm' className='relative'>
           <FontAwesomeIcon icon={faBell} className='h-4 w-4' />
-          {/* {unreadCount > 0 && (
-            <Badge
-              variant='destructive'
-              className='absolute -top-1 -right-1 h-5 w-5 rounded-full p-0 flex items-center justify-center text-xs'>
-              {unreadCount > 9 ? '9+' : unreadCount}
-            </Badge>
-          )} */}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end' className='min-w-96'>
