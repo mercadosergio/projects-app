a9e631fbc439a7f60d7b52934881e6b809bbbf73
Sergio Mercado <mercadosergio754@gmail.com>
2025-08-19 18:16:47 -0500

    fix env vars

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,6 +1,6 @@
 export const environment = {
   API_URL: 'http://localhost:3000/api',
-  AWS_REGION: process.env.AWS_REGION,
+  AWS_REGION: process.env.AWS3_REGION,
   AWS_ACCESS_KEY_ID: process.env.AWS3_ACCESS_KEY_ID,
   AWS_SECRET_KEY: process.env.AWS3_SECRET_KEY,
   DYNAMODB_TABLENAME: process.env.DYNAMODB_TABLENAME,
